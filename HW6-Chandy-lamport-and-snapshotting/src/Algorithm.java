3c272b8dc1eed19db85143437e0fb6862c1f434e
Mayuri Wadkar <mayuri-wad-012447851@users.noreply.github.com>
2017-10-13 13:22:31 -0700

    updating Chandy lamport Algorithm class

--- HW6-Chandy-lamport-and-snapshotting/src/Algorithm.java
+++ HW6-Chandy-lamport-and-snapshotting/src/Algorithm.java
@@ -10,50 +10,41 @@ public class Algorithm {
     Processor processor1, processor2, processor3;
 
     public Algorithm(Processor processor1, Processor processor2, Processor processor3) {
-    		this.processor1 = processor1;
-    		this.processor2 = processor2;
-    		this.processor3 = processor3;
+    	this.processor1 = processor1;
+		this.processor2 = processor2;
+		this.processor3 = processor3;
     }
 
-    public void executionPlanP1 () throws InterruptedException{
+    public void executionPlan1(){
+        compute(processor1);
+        compute(processor1);
+        compute(processor1);
+        compute(processor1);
         compute(processor1);
         compute(processor1);
         compute(processor1);
 /**
- * TODO: Homework: Implement send message from processor1 to different processors. Add a time gap betweeen two different
- *                send events. Add computation events between two different sends.
- *      [Hint: Create a loop that kills time, sleep , wait on some value etc..]
- *
+ * TODO: Homework: Implement send message from processor1 to different processors. Add a time gap between two different
+ *                send events. Add computation events between two diferent sends.
  */
-        for(Buffer c : processor1.outChannels) {
-        		Message m = new Message(MessageType.SEND,processor1);
-        		processor1.sendMessageTo(m, c);
-        		Thread.sleep(200);
-        }
+        processor1.sendMessgeTo(null,null);
+
+        processor1.sendMessgeTo(null,null);
+
+        processor1.sendMessgeTo(null,null);
+
     }
 
-    public void executionPlanP2() {
-        compute(processor2);
-        compute(processor2);
-        compute(processor2);
-		for (Buffer c : processor2.outChannels) {
-			processor2.sendMessageTo(new Message(MessageType.ALGORITHM,processor2), c);
-			processor2.sendMessageTo(new Message(MessageType.COMPUTATION,processor2), c);
-			processor2.sendMessageTo(new Message(MessageType.RECEIVE,processor2), c);
-			processor2.sendMessageTo(new Message(MessageType.SEND,processor2), c);
-		}
+    // Write hard coded execution plan for processors
+    public void executionPlanP1() {
+
+
     }
-    
-    public void executionPlanP3() {
-        compute(processor3);
-        compute(processor3);
-        compute(processor3);
-		for (Buffer c : processor3.outChannels) {
-			processor3.sendMessageTo(new Message(MessageType.ALGORITHM,processor3), c);
-			processor3.sendMessageTo(new Message(MessageType.SEND,processor3), c);
-			processor3.sendMessageTo(new Message(MessageType.RECEIVE,processor3), c);
-			processor3.sendMessageTo(new Message(MessageType.COMPUTATION,processor3), c);
-		}
+
+    // Write hard coded execution plan for processors
+    public void executionPlanP2() {
+
+
     }
 
     /**
@@ -61,7 +52,7 @@ public class Algorithm {
      * @param p
      */
     public void compute(Processor p) {
-        System.out.println("Compute event at Processor" + p.getId());
+        System.out.println("Doing some computation on " + p.getClass().getSimpleName());
     }
 
     /**
@@ -70,7 +61,7 @@ public class Algorithm {
      * @param channel the incoming channel on the to processor that will receive this message
      */
     public void send(Processor to, Buffer channel) {
-        to.sendMessageTo(new Message(MessageType.ALGORITHM,to), channel); // ALGORITHM
+        to.sendMessgeTo(null, channel); // ALGORITHM
     }
 
 }
