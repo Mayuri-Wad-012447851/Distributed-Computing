a9841bb6b94a6a5c9560f6dd297d697f585d4a14
Mayuri <mayuri@users.noreply.github.com>
2018-11-18 17:45:41 -0800

    Removing HMK7 & HMK8

--- HW7-Leader-election-async-On2/src/MessageType.java
+++
@@ -1,9 +0,0 @@
-/**
- * Each message being shared between processors is classified as TERMINATE or IDENTIFIER message
- * 
- * @author Mayuri Wadkar, Eric Han, Sonali Mishra
- *
- */
-public enum MessageType {
-	TERMINATE, IDENTIFIER
-}
